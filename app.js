83b79737aece9a8f10bd28a3da7a460e6e831adb
DanCNo <dan.noh@gmail.com>
2019-05-21 15:28:21 -0400

    implement test routes in app.js

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const bodyParser = require("body-parser");
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Greetings World"));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.Port || 5000;
 
