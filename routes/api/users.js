9181337577aa1cfbfbabc9050993b1ae565ff5c8
DanCNo <dan.noh@gmail.com>
2019-05-22 16:34:06 -0400

    add user route for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,4 +4,33 @@ const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route"}));
 
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+
+router.post("/register", (req, res) => {
+
+  User.findOne({ email: req.body.email })
+    .then( user => {
+      if( user ){
+        return res.status(400).json({email: "A user has already registered with this address"});
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then( user => res.json(user))
+              .catch( err => console.log(err));
+          });
+        });
+      }
+    });
+});
+
 module.exports = router;
